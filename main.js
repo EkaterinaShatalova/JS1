e4a274a96034ab62b63e3a1acda99ac8389ef116
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-07 17:26:08 +0300

    feature(main.js): bot created

---
+++ main.js
@@ -0,0 +1,40 @@
+/*jshint esversion: 8 */
+/* jshint node: true */
+'use strict';
+
+
+let num = function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+let bot1 = function() {
+    let task = num(1,100);
+    console.log(task);  //см число
+    function bot() {
+        let result = prompt('Угадай число от 1 до 100');
+        if (result === null) {
+            alert('Game over!');
+        } 
+        else if (result > 100 ||  result < 1 || (isNaN(parseFloat(result)) && !isFinite(result))) {
+            alert('Введите число от 1 до 100!!');
+            bot();
+        }
+        else if  (result < task ) {
+            alert('Загаданное число больше');
+            bot();
+        }
+        else if  (result > task ) {
+            alert('Загаданное число меньше');
+            bot();
+        }
+        else {
+            alert('Поздравляю, вы угадали!');
+        }
+    }  
+    bot(); 
+    console.dir(bot); //см замыкание    
+};
+
+bot1();
