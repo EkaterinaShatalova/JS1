196de31d6a21160402b83330ddbe41d48ccc480c
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-10 20:44:24 +0300

    fix(main.js): if else fixed

--- main.js
+++ main.js
@@ -2,22 +2,30 @@
 /* jshint node: true */
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const date = new Date();
 
 week.forEach(function(item) {
-    if (date.toLocaleString('ru', {weekday: 'long'}) === item.toLowerCase()) {
-        console.log(`%c${item}`, 'font-weight: bold');
-
-    } else if (item === 'Суббота' || item === 'Воскресенье' ) {
+    if (item === 'Суббота' || item === 'Воскресенье' ) {
         if (date.toLocaleString('ru', {weekday: 'long'}) === item.toLowerCase()) {
             console.log(`%c${item}`, 'font-weight: bold; font-style: italic');
         }
         else {
             console.log(`%c${item}`, 'font-style: italic');
+        }}
+    else {
+        if(date.toLocaleString('ru', {weekday: 'long'}) === item.toLowerCase()) {
+            console.log(`%c${item}`, 'font-weight: bold');
         }
-    } else {
-        console.log(item);
+        else {
+            console.log(item);
+        }
     }
 }
 );
+
+
+
+    
+
+
