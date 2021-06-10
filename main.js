1d3985f0e32c5e3b0c0d6d79f505cb298404a3d3
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-10 11:51:40 +0300

    feature(script.js): week created & iterated

--- main.js
+++ main.js
@@ -2,55 +2,22 @@
 /* jshint node: true */
 'use strict';
 
-const num = function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const date = new Date();
 
-const bot1 = function() {
-    const task = num(1,100);
-    let attempt = 10;
-    console.log(task);  //см число
-    function bot() {
-        let result = prompt('Угадай число от 1 до 100');
-        if (result === null) {
-            alert('Game over!');
-        } 
-        else if ( isNaN(parseFloat(result)) || !isFinite(result) || result < 1 || result > 100 || (parseFloat(result) !== parseInt(result))) {
-            alert('Введите число от 1 до 100!!');
-            bot();
-        }
-        else if  (result < task || result > task) {
-            attempt --;
-            if (attempt === 0) {
-                let losing = confirm('Попытки закончились, хотите сыграть еще?');
-                if (losing) {
-                    bot1();                   
-                }
-                else {
-                    alert('Спасибо за участие!');
-                }
-            }
-            else {
-                result < task? alert(`Загаданное число больше, осталось попыток: ${attempt}`) : alert(`Загаданное число меньше, осталось попыток: ${attempt}`)
-                bot();
-            }
-        }
-        else if (result == task) {
-            let result1 = confirm('Поздравляю, вы угадали! Хотели бы сыграть еще?');
-            if (result1) {
-                bot1();
-            }
-            else {alert('Спасибо за участие!');
-            }           
+week.forEach(function(item) {
+    if (date.toLocaleString('ru', {weekday: 'long'}) === item.toLowerCase()) {
+        console.log(`%c${item}`, 'font-weight: bold');
+
+    } else if (item === 'Суббота' || item === 'Воскресенье' ) {
+        if (date.toLocaleString('ru', {weekday: 'long'}) === item.toLowerCase()) {
+            console.log(`%c${item}`, 'font-weight: bold; font-style: italic');
         }
         else {
-            alert('Что-то не так :(');
-        }       
+            console.log(`%c${item}`, 'font-style: italic');
+        }
+    } else {
+        console.log(item);
     }
-    bot();
-    console.dir(bot); //см замыкание
-};
-
-bot1();
+}
+);
