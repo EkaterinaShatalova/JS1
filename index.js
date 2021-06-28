048206155ea1b4092bf6ad38b896588ba59ec1a6
EkaterinaShatalova <c.shatalova@mail.ru>
2021-06-28 20:55:02 +0300

    feature(index.js): random Color added

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 /*jshint esversion: 8 */
 /* jshint node: true */
+
 'use strict';
 
-function get_random_color() 
-{
+const randomColor = function () {
     let color = "";
     for(let i = 0; i < 3; i++) {
-        const sub = Math.floor(Math.random() * 256).toString(16);
-        console.log(sub);
-        color += (sub.length == 1 ? "0" + sub : sub);
-        console.log(color);
+        const a = Math.floor(Math.random() * 256).toString(16);
+        color += (a.length == 1 ? "0" + a : a);
     }
     return "#" + color;
-}
+};
 
 const btn = document.getElementById('add');
-const color = document.getElementById('color')
+const color = document.getElementById('color');
 
 btn.addEventListener('click', ()=> {
-    document.body.style.background = get_random_color();
-    color.innerText = get_random_color();
+    document.body.style.background = randomColor();
+    color.innerText = randomColor();
 });
 
